263536882d4dc2f767f735fe85495df2ac7c1a21
Tova Jertfelt <tovizitan@gmail.com>
2021-12-14 11:36:44 +0100

    halfway done with homepage

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-console.log("Test");
+document.documentElement.classList.add('preparation');
+
+window.onload = function() {
+  document.documentElement.classList.remove('preparation');
+};
 
 //Menu button mobile
 let menuButton = document.getElementById("menutoggle");
 let menuMobile = document.querySelector("#drop-down");
 
 menuButton.addEventListener("click", dropdown);
-console.log("menu")
+
 function dropdown(){
-  
  if (menuMobile.classList.contains("drop-down"))
  {menuMobile.classList.remove("drop-down");
 }
