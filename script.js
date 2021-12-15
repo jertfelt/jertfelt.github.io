bb0c1d9903b28fd1c72c963dfe749ca53b990dbf
Tova Jertfelt <tovizitan@gmail.com>
2021-12-15 12:41:21 +0100

    bytt bilder

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ function dropdown(){
 else 
 menuMobile.classList.add("drop-down");
 }
+
+
+let slideIndex = 0;
+carousel();
+
+function carousel() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 6000); 
+}
