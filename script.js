f9a1fd3e3488e07d10fd1a0ada2751a2ee123c5c
Tova Jertfelt <tovizitan@gmail.com>
2021-12-19 13:01:21 +0100

    cv klar! homepage bugs. starting with cases

--- script.js
+++ script.js
@@ -13,20 +13,3 @@ else
 menuMobile.classList.add("drop-down");
 }
 
-//Slides with quotes
-let slideIndex = 0;
-carousel();
-
-function carousel() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 6000); 
-}
-
-//cv 
