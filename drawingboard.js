f65b877618fa816370d941fe3457f4ff585b91d0
Tova Jertfelt <tovizitan@gmail.com>
2022-01-14 11:00:34 +0100

    trying new

--- drawingboard.js
+++ drawingboard.js
@@ -2,21 +2,7 @@
 
 let canvas = document.getElementById("drawingboard");
 
-canvas.width = window.innerWidth;
-// canvas.height = window.innerHeight;
-
-let toolDrawing = canvas.getContext("2d");
-
-let prevMouseX = null;
-let prevMouseY = null;
-let prevTouchX = null;
-let prevTouchY = null;
-
-toolDrawing.lineWidth = 5;
-
-let draw = false;
-
-
+//*buttons:
 let clearAllBtn = document.querySelector(".clear");
 
 clearAllBtn.addEventListener("click", () => {
@@ -65,12 +51,14 @@ saving();
   }
 })
 
+//*pen sizes
 let penModal = document.getElementById("pennstorlek--modal");
 let penClose = document.getElementsByClassName("pennstorlek__close")[0];
 
 let penSizeButton = document.querySelector(".width");
 penSizeButton.addEventListener("click", () => {
   penModal.style.display ="block";
+
 
 let penButtonSmall = document.getElementById("pennstorlek--liten");
 penButtonSmall.addEventListener("click", () => {
@@ -101,65 +89,160 @@ penButtonGig.addEventListener("click", () =>{
   penModal.style.display ="none";};
 })
 
-// let pennButtons = document.querySelectorAll(".pennstorlek--button");
-
-
-  // if (toolDrawing.lineWidth === 5){
-  // toolDrawing.lineWidth = 10;
-  // }
-  // else {
-  //   toolDrawing.lineWidth = 5;
-  // }
 })
 
 penClose.addEventListener("click", () =>{
   penModal.style.display ="none";
 })
 
-window.addEventListener("mousedown", (e) => draw = true);
-window.addEventListener("mouseup", (e) => draw = false);
-window.addEventListener("touchstart", (e) => draw = true);
-window.addEventListener("touchend", (e) => draw =false);
 
-window.addEventListener("mousemove", (e) => {
-    if(prevMouseX == null || prevMouseY == null || !draw){
-      prevMouseX = e.clientX;
-      prevMouseY = e.clientY;
-        return;
-    }
-    
-    let currentMouseX = e.clientX;
-    let currentMouseY = e.clientY;
+//*drawing
 
-    toolDrawing.beginPath();
-    toolDrawing.moveTo(prevMouseX, prevMouseY);
-    toolDrawing.lineTo(currentMouseX, currentMouseY);
-    toolDrawing.stroke()
+canvas.width = window.innerWidth;
+// // canvas.height = window.innerHeight;
 
-    prevMouseX = currentMouseX;
-    prevMouseY = currentMouseY;
-})
+var canvas = document.createElement('canvas');
+canvas.id = "canvas";
+document.body.appendChild(canvas);
 
-window.addEventListener("touchmove", (e) => {
-  if (prevTouchX ==null|| prevTouchY == null || !draw ){
-    prevTouchX = e.clientX;
-    prevTouchY = e.clientY;
-    return;
+document.body.style.margin = 0;
+canvas.style.position = 'relative';
+
+var ctx = canvas.getContext('2d');
+resize();
+
+var pos = {
+  x: 0,
+  y: 0
+};
+var buttonDown = false;
+
+const cvs = document.getElementById("canvas");
+cvs.addEventListener('mousedown', getRandomInt);
+cvs.addEventListener('touchstart', getRandomInt);
+
+window.addEventListener('resize', resize);
+
+document.addEventListener('mousemove', draw);
+document.addEventListener('mousedown', setPosition);
+document.addEventListener('mouseup', released);
+
+document.addEventListener('touchstart', setPosition);
+document.addEventListener('touchmove', draw);
+document.addEventListener('touchend', released);
+
+function getRandomInt() {
+  window.randInt = Math.floor(Math.random() * Math.floor(3));
+}
+
+function released(e) {
+  buttonDown = false;
+}
+
+function setPosition(e) {
+  if (e.type == "touchstart" || e.type == "mousedown") {
+    buttonDown = true;
+  }
+  if (e.type == "touchstart" || e.type == "touchmove") {
+    pos.x = e.touches[0].clientX;
+    pos.y = e.touches[0].clientY;
+  } else {
+    pos.x = e.clientX;
+    pos.y = e.clientY;
+  }
+}
+
+function resize() {
+  ctx.canvas.width = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
+}
+
+function draw(e) {
+  if (!buttonDown) return;
+
+  var color = '';
+
+  switch (window.randInt) {
+    case 1:
+      color = 'green';
+      break;
+    case 2:
+      color = 'red';
+      break;
+    case 0:
+      color = 'blue';
+      break;
   }
 
-  let currentTouchX = e.clientX;
-  let currentTouchY = e.clientY;
+  ctx.beginPath();
 
-  toolDrawing.beginPath();
-  toolDrawing.moveTo(prevTouchX, prevTouchY);
-  toolDrawing.lineTo(currentTouchX, currentTouchY);
-  toolDrawing.stroke()
+  ctx.lineWidth = 3;
+  ctx.lineCap = 'round';
+  ctx.strokeStyle = color;
 
-  prevTouchX = currentTouchX;
-  prevTouchY = currentTouchY;
+  ctx.moveTo(pos.x, pos.y);
+  setPosition(e);
+  ctx.lineTo(pos.x, pos.y);
+
+  ctx.stroke();
+}
+
+// let toolDrawing = canvas.getContext("2d");
+
+// let prevMouseX = null;
+// let prevMouseY = null;
+// let prevTouchX = null;
+// let prevTouchY = null;
+
+// toolDrawing.lineWidth = 5;
+
+// let draw = false;
+
+// window.addEventListener("mousedown", (e) => draw = true);
+// window.addEventListener("mouseup", (e) => draw = false);
+// window.addEventListener("touchstart", (e) => draw = true);
+// window.addEventListener("touchend", (e) => draw =false);
+
+// window.addEventListener("mousemove", (e) => {
+//     if(prevMouseX == null || prevMouseY == null || !draw){
+//       prevMouseX = e.clientX;
+//       prevMouseY = e.clientY;
+//         return;
+//     }
+    
+//     let currentMouseX = e.clientX;
+//     let currentMouseY = e.clientY;
+
+//     toolDrawing.beginPath();
+//     toolDrawing.moveTo(prevMouseX, prevMouseY);
+//     toolDrawing.lineTo(currentMouseX, currentMouseY);
+//     toolDrawing.stroke()
+
+//     prevMouseX = currentMouseX;
+//     prevMouseY = currentMouseY;
+// })
+
+// window.addEventListener("touchmove", (e) => {
+//   if (prevTouchX ==null|| prevTouchY == null || !draw ){
+//     prevTouchX = e.clientX;
+//     prevTouchY = e.clientY;
+//     return;
+//   }
+
+//   let currentTouchX = e.clientX;
+//   let currentTouchY = e.clientY;
+
+//   toolDrawing.beginPath();
+//   toolDrawing.moveTo(prevTouchX, prevTouchY);
+//   toolDrawing.lineTo(currentTouchX, currentTouchY);
+//   toolDrawing.stroke()
+
+//   prevTouchX = currentTouchX;
+//   prevTouchY = currentTouchY;
   
-})
+// })
 
+//*shortcut modal
 let shortcutModal = document.getElementById("drawingboard__shortcuts");
 let shortcutButt = document.getElementsByClassName("question")[0];
 let shortcutClose = document.getElementsByClassName("pennstorlek__close")[1];
