9b8e4672a2889b099676f1dd5e55c84283616473
Tova Jertfelt <tovizitan@gmail.com>
2022-01-14 11:12:08 +0100

    testing again

--- drawingboard.js
+++ drawingboard.js
@@ -99,93 +99,7 @@ penClose.addEventListener("click", () =>{
 //*drawing
 
 canvas.width = window.innerWidth;
-// // canvas.height = window.innerHeight;
-
-var canvas = document.createElement('canvas');
-canvas.id = "canvas";
-document.body.appendChild(canvas);
-
-document.body.style.margin = 0;
-canvas.style.position = 'relative';
-
-var ctx = canvas.getContext('2d');
-resize();
-
-var pos = {
-  x: 0,
-  y: 0
-};
-var buttonDown = false;
-
-const cvs = document.getElementById("canvas");
-cvs.addEventListener('mousedown', getRandomInt);
-cvs.addEventListener('touchstart', getRandomInt);
-
-window.addEventListener('resize', resize);
-
-document.addEventListener('mousemove', draw);
-document.addEventListener('mousedown', setPosition);
-document.addEventListener('mouseup', released);
-
-document.addEventListener('touchstart', setPosition);
-document.addEventListener('touchmove', draw);
-document.addEventListener('touchend', released);
-
-function getRandomInt() {
-  window.randInt = Math.floor(Math.random() * Math.floor(3));
-}
-
-function released(e) {
-  buttonDown = false;
-}
-
-function setPosition(e) {
-  if (e.type == "touchstart" || e.type == "mousedown") {
-    buttonDown = true;
-  }
-  if (e.type == "touchstart" || e.type == "touchmove") {
-    pos.x = e.touches[0].clientX;
-    pos.y = e.touches[0].clientY;
-  } else {
-    pos.x = e.clientX;
-    pos.y = e.clientY;
-  }
-}
-
-function resize() {
-  ctx.canvas.width = window.innerWidth;
-  ctx.canvas.height = window.innerHeight;
-}
-
-function draw(e) {
-  if (!buttonDown) return;
-
-  var color = '';
-
-  switch (window.randInt) {
-    case 1:
-      color = 'green';
-      break;
-    case 2:
-      color = 'red';
-      break;
-    case 0:
-      color = 'blue';
-      break;
-  }
-
-  ctx.beginPath();
-
-  ctx.lineWidth = 3;
-  ctx.lineCap = 'round';
-  ctx.strokeStyle = color;
-
-  ctx.moveTo(pos.x, pos.y);
-  setPosition(e);
-  ctx.lineTo(pos.x, pos.y);
-
-  ctx.stroke();
-}
+// canvas.height = window.innerHeight;
 
 // let toolDrawing = canvas.getContext("2d");
 
@@ -197,6 +111,11 @@ function draw(e) {
 // toolDrawing.lineWidth = 5;
 
 // let draw = false;
+
+// window.addEventListener("touchstart", function(e){
+//   console.log("event start");
+// });
+
 
 // window.addEventListener("mousedown", (e) => draw = true);
 // window.addEventListener("mouseup", (e) => draw = false);
