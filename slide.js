06287ecf22ca2d191e62a96fa0447608a1c0065e
Tova Jertfelt <tovizitan@gmail.com>
2022-03-02 14:25:13 +0100

    preppar js för home

---
+++ slide.js
@@ -0,0 +1,15 @@
+  
+let slideIndex = 0;
+carousel();
+
+function carousel() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 6000); 
+}
