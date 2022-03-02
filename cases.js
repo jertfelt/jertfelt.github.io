7fadd2718b54450a35c4722c317181f9b2fea29d
Tova Jertfelt <tovizitan@gmail.com>
2022-03-02 14:23:46 +0100

    fixat javascript för cases

--- cases.js
+++ cases.js
@@ -1,10 +1,12 @@
 
 //*---variables
-const caseSectionAll = document.getElementsByClassName("cases__all");
+const caseSectionAll = document.getElementsByClassName("cases__all")[0];
 console.log(caseSectionAll);
 
 const caseArticleElmen = document.createElement("article");
-caseArticleElmen.classList.add("cases__article"); 
+caseArticleElmen.classList.add("cases__article");
+
+caseSectionAll.appendChild(caseArticleElmen);
 
 let casesArray = [];
 
@@ -16,44 +18,55 @@ const drawCases = (casesInfo) => {
 
   const articleContent = document.createElement("div");
   articleContent.classList.add("cases__article--content");
-  articleContent.classList.add("container--large");
-  articleContent.classList.add("padding--m");
+  // articleContent.classList.add("container--large");
+  // articleContent.classList.add("padding--m");
+  const hrContent = document.createElement("hr");
+  caseArticleElmen.appendChild(hrContent);
 
   articleContent.innerHTML= `
-  <hr>
   <div class="cases__article--column">
-  <h2 class="text--light">"${item.headings.title}" </h2>
-  <h3 class ="text--light">"${item.description.subject}"</h3> 
-    <a href="${item.sources.link}" target="_blank">
-      <img class="drop--blue border-item--third"
+  <span>  <h3 class="text--light text--bold ">
+  ${item.headings.title} </h3>
+  <h4 class ="text--white">
+  ${item.description.subject}</h4>
+  </span>
+  <div class="cases__article--imgcolumn">
+  <a href="
+    ${item.sources.link}"
+    target="_blank">
+      <img class="cases__img"
       src="${item.sources.image.url}"
-      alt ="${item.sources.imagealt}"
+      alt="${item.sources.imagealt}"
       title="Gå till sidan"
       aria-label="Go to page"></img>
     </a>
+    </div>
+
   </div>
 
-  <div class="cases__article">
-  <a class="text--xxs text--light text--bold"
+  <div class="cases__article--right">
+  <a class="text--s text--light"
   target="_blank"
-  href="${item.sources.link}">
+  href=${item.sources.link}>
   Se projektet här</a>
-  <p class="text--white text--xs text--width70 margin-top-xxsmall">
+  <p class=" text--light text--xxs cases__tags">${item.description.tag}</p>
+  <p class="text--white text--xs">
   ${item.description.text}
   </p>
-  <br/>
+  <br>
   <a href="${item.sources.githublinks.github}"
   target="_blank"
-  class="text--light text--xxs text--center text--bold">
-  Github</a>
+  class="text--light text--xs text--center text--bold link--margin">
+  <button class="button--square">
+  Kolla in projektet på github</button></a>
   </div>
+
   `
   caseArticleElmen.appendChild(articleContent);
 
 })
 
 }
-
 
 //*----fetching json
 async function fetchingJson() {
@@ -68,3 +81,14 @@ drawCases(caseDraw);
 }
 
 fetchingJson();
+
+
+/* <a href="
+${item.sources.link}"
+target="_blank">
+  <img class="drop--blue border-item--third"
+  src="${item.sources.image.url}"
+  alt="${item.sources.imagealt}"
+  title="Gå till sidan"
+  aria-label="Go to page"></img>
+</a> */
--- script.js
+++ script.js
@@ -13,3 +13,17 @@ else
 menuMobile.classList.add("drop-down");
 }
 
+
+//*----fetching json
+async function fetchingJson() {
+  const response = await fetch("./js/data/cases.json");
+  const casesInfo = await response.json();
+  casesArray = [...casesInfo.cases];
+  console.log(casesArray);
+  const caseDraw = casesArray.map(item => {
+    return item;
+  })
+  drawCases(caseDraw);
+  }
+  
+  fetchingJson();
